c6d5752489cf03d01e7bc721caade52a4242e99f
Colisian McLeod <mcleodom16@gmail.com>
2023-06-21 09:04:37 -0400

    Creating AddressBook class. Added comments.

--- src/main/java/org/example/AddressBook.java
+++ src/main/java/org/example/AddressBook.java
@@ -20,11 +20,14 @@ public class AddressBook {
         System.out.println("Contact deleted: " + contact);
     }
     public void updateContact(AddressContacts outdateContact, AddressContacts newContact){
+        //Uses indexof to find the out of date contact in the contacts list, if it is not found it will return -1
         int index = contacts.indexOf(outdateContact);
         if (index != -1){
+            //old contact is found in the address book replace it with the new one.
             contacts.set(index, newContact);
             System.out.println("Contact updated: " + outdateContact + " -> " + newContact);
         } else {
+            //if the old contact is not found display message
             System.out.println("Contact not found: " + outdateContact);
         }
     }
