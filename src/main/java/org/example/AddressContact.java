424bd1de76ce0db1cc14a768708c95ca51ab9008
Colisian McLeod <mcleodom16@gmail.com>
2023-06-21 17:06:08 -0400

    Creating AddressContact class. Needs Comments. Implemented Builder Design pattern.

--- src/main/java/org/example/AddressContact.java
+++ src/main/java/org/example/AddressContact.java
@@ -1,5 +1,5 @@
 package org.example;
-
+// Attributes to create the object
 public class AddressContact {
     private String name;
     private String street;
@@ -8,65 +8,73 @@ public class AddressContact {
     private  String  zipCode;
     private  String phoneNumber;
 
-    public AddressContact(String name, String street, String city, String state, String zipCode, String phoneNumber){
-        this.name = name;
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
-        this.phoneNumber = phoneNumber;
+    public AddressContact(Builder builder){
+        this.name = builder.name;
+        this.street = builder.street;
+        this.city = builder.city;
+        this.state = builder.state;
+        this.zipCode = builder.zipCode;
+        this.phoneNumber = builder.phoneNumber;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getStreet() {
         return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getState() {
         return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 
     public String getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-    @Override
-    public String toString(){
-        return "Name: " +  name + ", Street; " + street + ", City: " + city + ", Street: " + ", ZipCode: " + zipCode +
-                ", phone number: " + phoneNumber;
+    public static class Builder {
+        private String name;
+        private String street;
+        private String city;
+        private  String state;
+        private  String  zipCode;
+        private  String phoneNumber;
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder street(String street) {
+            this.street = street;
+            return this;
+        }
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+        }
+        public Builder state(String state) {
+            this.state = state;
+            return this;
+        }
+        public Builder zipCode(String zipCode) {
+            this.zipCode = zipCode;
+            return this;
+        }
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+        public AddressContact build(){
+            return new AddressContact(this);
+        }
     }
 }
