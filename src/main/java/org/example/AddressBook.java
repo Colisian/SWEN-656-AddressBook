70621c101f8b10514c526c31cc0bcfa59faf6d9c
Colisian McLeod <mcleodom16@gmail.com>
2023-06-21 08:39:34 -0400

    Creating AddressBook class. Added add, delete and update methods.

--- src/main/java/org/example/AddressBook.java
+++ src/main/java/org/example/AddressBook.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Hello! Hi");
+    private List<AddressContacts> contacts;
+
+    public AddressBook(){
+        contacts = new ArrayList<>();
     }
+
+    public void addContact(AddressContacts contact){
+        contacts.add(contact);
+        System.out.println("Contact added: " + contact);
+    }
+
+    public void deleteContact(AddressContacts contact){
+        contacts.remove(contact);
+        System.out.println("Contact deleted: " + contact);
+    }
+    public void updateContact(AddressContacts outdateContact, AddressContacts newContact){
+        int index = contacts.indexOf(outdateContact);
+        if (index != -1){
+            contacts.set(index, newContact);
+            System.out.println("Contact updated: " + outdateContact + " -> " + newContact);
+        } else {
+            System.out.println("Contact not found: " + outdateContact);
+        }
+    }
+
 }
