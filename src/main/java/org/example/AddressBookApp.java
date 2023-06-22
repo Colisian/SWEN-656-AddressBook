185c5b5c56336e20fab1abff9e5acbbaa5018314
Colisian McLeod <mcleodom16@gmail.com>
2023-06-22 07:45:44 -0400

    Ran test for AddressBookApp. Getting Error: "ajc: can't find referenced pointcut updateContact"

--- src/main/java/org/example/AddressBookApp.java
+++ src/main/java/org/example/AddressBookApp.java
@@ -3,6 +3,14 @@ package org.example;
 
 public class AddressBookApp {
     public static void main(String[] args) {
-        ;
+
+        AddressBook addressBook = new AddressBook();
+        AddressContact contact = new AddressContact.Builder()
+                .name("John Doe")
+                .street("11420 Old Frederick")
+                .city("Prince Georges")
+                .state("MD")
+                .zipCode("21103")
+                .phoneNumber("4438502211").build();
     }
 }
